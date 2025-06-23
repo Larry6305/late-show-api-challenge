fc21da5b2b28888c4b1dcffee0369c39d648f9d6
Larry <melarry46@gmail.com>
2025-06-23 09:36:22 +0300

    done

--- server/controllers/episode_controller.py
+++ server/controllers/episode_controller.py
@@ -1,18 +1,38 @@
 from flask import Blueprint, jsonify
-from flask_jwt_extended import jwt_required
 from ..models.episode import Episode
 from ..app import db
 
 episode_bp = Blueprint("episode", __name__, url_prefix="/episodes")
 
-@episode_bp.route("/<int:id>", methods=["DELETE"])
-@jwt_required()
-def delete_episode(id):
+@episode_bp.route("", methods=["GET"])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([
+        {
+            "id": e.id,
+            "date": e.date.isoformat(),
+            "number": e.number
+        } for e in episodes
+    ])
+
+@episode_bp.route("/<int:id>", methods=["GET"])
+def get_episode(id):
     episode = Episode.query.get(id)
     if not episode:
         return jsonify({"error": "Episode not found"}), 404
 
-    db.session.delete(episode)
-    db.session.commit()
-
-    return jsonify({"message": "Episode deleted"}), 200
+    return jsonify({
+        "id": episode.id,
+        "date": episode.date.isoformat(),
+        "number": episode.number,
+        "appearances": [
+            {
+                "id": a.id,
+                "rating": a.rating,
+                "guest": {
+                    "id": a.guest.id,
+                    "name": a.guest.name
+                }
+            } for a in episode.appearances
+        ]
+    })
