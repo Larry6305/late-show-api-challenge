6915ea12399bd201d9cf1ec92fade7400adda64c
Larry <melarry46@gmail.com>
2025-06-23 08:39:35 +0300

    models

---
+++ server/models/appearance.py
@@ -0,0 +1,15 @@
+from ..app import db
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    # Validation: 1-5 rating
+    @staticmethod
+    def validate_rating(rating):
+        return 1 <= rating <= 5
